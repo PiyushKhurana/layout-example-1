82c653185c695d8cf0ad7ecc6ed0a14355dd94b6
PiyushKhurana <khuranapiyush27@gmail.com>
2019-07-22 19:23:55 +0530

    image section complete

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         ),
         body:ListView(
           children: <Widget>[
+            ImageSection(),
             TitleSection(),
             TextSection(),
           ],
