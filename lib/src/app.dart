77f8d5557d2f851182336045b06549c43e11644f
PiyushKhurana <khuranapiyush27@gmail.com>
2019-07-22 17:55:22 +0530

    import statements

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import './widgets/button_section.dart';
+import './widgets/text_section.dart';
+import './widgets/title_section.dart';
+
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
